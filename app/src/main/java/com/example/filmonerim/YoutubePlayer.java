bf8aa30daa1c4b17ed6d06ac8fe28ae734c08a0c
Yasin KAYA <58563631+yasinkya@users.noreply.github.com>
2021-05-30 17:56:09 +0300

    play with id

--- app/src/main/java/com/example/filmonerim/YoutubePlayer.java
+++ app/src/main/java/com/example/filmonerim/YoutubePlayer.java
@@ -5,6 +5,10 @@ import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
 public class YoutubePlayer extends AppCompatActivity {
 
     @Override
@@ -14,5 +18,16 @@ public class YoutubePlayer extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        YouTubePlayerView player = findViewById(R.id.ytbPlayer);
+        getLifecycle().addObserver(player);
+
+        player.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer) {
+                youTubePlayer.loadVideo("zT38i4eF2cE",0);
+            }
+        });
     }
 }
