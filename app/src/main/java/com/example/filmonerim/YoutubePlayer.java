1f60e33a0f998528f765fd4a7abaf9bf2f05b87a
Yasin KAYA <58563631+yasinkya@users.noreply.github.com>
2021-05-30 16:02:53 +0300

    youtube player api added

---
+++ app/src/main/java/com/example/filmonerim/YoutubePlayer.java
@@ -0,0 +1,14 @@
+package com.example.filmonerim;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class YoutubePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_player);
+    }
+}
