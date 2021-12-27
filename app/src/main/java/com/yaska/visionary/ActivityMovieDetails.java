f7c816d253f509cd991acded0f7c45e71f10b09f
Yasin KAYA <yasinkaya1007@gmail.com>
2021-12-27 21:49:06 +0300

    moviedetails class funcs

--- app/src/main/java/com/yaska/visionary/ActivityMovieDetails.java
+++ app/src/main/java/com/yaska/visionary/ActivityMovieDetails.java
@@ -3,6 +3,7 @@ package com.yaska.visionary;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -12,8 +13,10 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTube
 public class ActivityMovieDetails extends AppCompatActivity {
 
     YouTubePlayerView player;
-    TextView movieName, description;
+    TextView et_movieName, et_description;
     AppCompatButton btn_play, btn_addfav;
+    String movieName, movieImage, movieId, movieVideoId;
+    boolean added_fav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,45 @@ public class ActivityMovieDetails extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         player = findViewById(R.id.mini_player);
-        movieName = findViewById(R.id.et_movieName);
-        description = findViewById(R.id.et_description);
+        et_movieName = findViewById(R.id.et_movieName);
+        et_description = findViewById(R.id.et_description);
         btn_play = findViewById(R.id.button_play);
         btn_addfav = findViewById(R.id.button_addfav);
+
+        movieName = getIntent().getStringExtra("movieName");
+        movieImage = getIntent().getStringExtra("movieImageUrl");
+        movieId= getIntent().getStringExtra("movieId");
+        movieVideoId = getIntent().getStringExtra("movieFileUrl");
+
+        et_movieName.setText(movieName);
+        et_description.setText("deneme description");
+
+        // glide internetten resim çekip image viewe aktar
+//        Glide.with(this).load(movieImage).into()
+
+        btn_play.setOnClickListener(v ->{
+            Intent intent = new Intent(ActivityMovieDetails.this, ActivityPlayer.class);
+            intent.putExtra("videoId", movieVideoId);
+            startActivity(intent);
+
+        });
+
+        btn_addfav.setOnClickListener(v ->{
+            //todo add to users favorites database
+            // ref.child("favorites").child(moviename).setvalue(movieclass)
+//            btn_addfav.setBackground(R.drawable.icon_addedfav);
+            if (!added_fav){
+                btn_addfav.setBackgroundResource(R.drawable.icon_addedfav);
+                added_fav = true;
+            }
+            else{
+                btn_addfav.setBackgroundResource(R.drawable.icon_addfav);
+                added_fav = false;
+            }
+
+
+        });
+
 
     }
 }
